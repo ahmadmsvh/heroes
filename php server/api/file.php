63b813bd57d5bec664605fe8e5d5e3eba5f1115c
Mohammad Norouzi Tafeshi <mn.tafreshi@gmail.com>
2023-06-11 12:23:00 +0330

    upload

---
+++ php server/api/file.php
@@ -0,0 +1,20 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+?>
+{
+"obj": [
+      {
+        "name": "ahmad",
+        "status": "online"
+      },
+      {
+        "name": "ali",
+        "status": "offline"
+      },
+      {
+        "name": "sam",
+        "status": "online"
+      }
+    ]
+}
